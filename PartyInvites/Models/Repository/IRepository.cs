7efaa3e47c7a34b0733eacfbd4f412822eef5a6b
tonousa <tonousa@hotmail.com>
2019-04-19 00:33:16 -0400

    pg 253

---
+++ PartyInvites/Models/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models.Repository
+{
+    public interface IRepository
+    {
+        IEnumerable<GuestResponse> GetAllResponses();
+
+        void AddResponse(GuestResponse response);
+    }
+}
---
+++ PartyInvites/Models/Repository/MemoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models.Repository
+{
+    public class MemoryRepository : IRepository
+    {
+        void IRepository.AddResponse(GuestResponse response)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<GuestResponse> IRepository.GetAllResponses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
